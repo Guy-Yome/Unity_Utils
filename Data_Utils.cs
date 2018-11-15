00a7e9b327e6dba3ca98fbafcb6e083683c5a92e
guillaume leduc <guillaume leduc>
2018-11-15 11:26:29 -0500

    Unmixed the data function that was in the math_utils.cs

---
+++ Data_Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Data_Utils {
+	/**
+    * This function shuffles a list of any type and changes its value directly.
+    *
+    * Source : https://answers.unity.com/questions/486626/how-can-i-shuffle-alist.html
+    */
+    public static void Shuffle<T> (this IList<T> list) {
+        for (int i = 0; i < list.Count; i++) {
+             T temp = list[i];
+             int random_index = UnityEngine.Random.Range(i, list.Count);
+             list[i] = list[random_index];
+             list[random_index] = temp;
+        }
+    }
+}
--- Math_Utils.cs
+++ Math_Utils.cs
@@ -46,18 +46,4 @@ public static class Math_Utils {
 			return new Vector2(0F, 0F);
 		}
 	}
-
-	/**
-	* This function shuffles a list of any type and changes its value directly.
-	*
-	* Source : https://answers.unity.com/questions/486626/how-can-i-shuffle-alist.html
-	*/
-	public static void Shuffle<T> (this IList<T> list) {
-	    for (int i = 0; i < list.Count; i++) {
-	         T temp = list[i];
-	         int random_index = UnityEngine.Random.Range(i, list.Count);
-	         list[i] = list[random_index];
-	         list[random_index] = temp;
-	    }
-	}
 }
